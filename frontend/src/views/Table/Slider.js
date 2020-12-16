2ba11de73196767ccbda38a12a6b5e64ad5d1fa0
Jovin <jovin.war@gmail.com>
2020-12-16 22:03:06 +0530

    change top product display styles

--- frontend/src/views/Table/Slider.js
+++ frontend/src/views/Table/Slider.js
@@ -1,83 +1,112 @@
-import React from 'react';
-import Carousel from "react-material-ui-carousel"
+import React from "react";
+import Carousel from "react-material-ui-carousel";
 
-import {
-    Card,
-    CardMedia,
-    Typography,
-    Grid,
-    Button,
-} from '@material-ui/core';
+import { Card, CardMedia, Typography, Grid, Button, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles(() => ({
+    banner: { height: "15rem", position: "relative" },
+    bannerGrid: { height: "80%" },
+    media: {
+      backgroundColor: "white",
+      height: "100%",
+      overflow: "hidden",
+      position: "relative",
+    },
+    mediaCaption: {
+      textOverflow: "ellipsis",
+      position: "absolute",
+      bottom: 0,
+      backgroundColor: "black",
+      color: "white",
+      opacity: " 0.6",
+      width: "100%",
+      height: "15%",
+      font: {
+        size: "2rem",
+        weight: 200,
+      },
+
+      transition: "300ms",
+      cursor: "pointer",
+    },
+  }));
 function Banner(props) {
-    const totalItems = props.item.length ? props.item.length : 0;
+  const totalItems = props.item.length ? props.item.length : 0;
 
-    let items = [];
+  let items = [];
 
+  const styles = useStyles()
 
+  for (let i = 0; i < totalItems; i++) {
+    const item = props.item[i];
 
-    for (let i = 0; i < totalItems; i++) {
-        const item = props.item[i];
+    const media = (
+      <Grid item key={item.name}>
+        <CardMedia
+          className={styles.media}
+          image={item.image}
+          title={item.name}
+        >
+          <Typography className={styles.mediaCaption}>{item.name}</Typography>
+        </CardMedia>
+        <Button
+          variant="outlined"
+          onClick={() => props.onClick(item)}
+          className="ViewButton"
+        >
+          Add to order 
+        </Button>
+      </Grid>
+    );
 
-        const media = (
-            <Grid  item key={item.name}>
-                <CardMedia
-                    className="Media"
-                    image={item.image}
-                    title={item.name}
-                >
-                    <Typography className="MediaCaption">
-                        {item.name}
-                    </Typography>
-                </CardMedia>
-                <Button variant="outlined" onClick={()=> props.onClick(item)} className="ViewButton">
-Add to order                </Button>
-            </Grid>
-        )
+    items.push(media);
+  }
 
-        items.push(media);
-    }
-
-
-    return (
-        <Card  style={{margin:'1rem'}} raised className="Banner">
-            <Grid style={{margin:'1rem'}} container spacing={6} className="BannerGrid">
-                {items}
-            </Grid>
-        </Card>
-    )
+  return (
+    <Card style={{ margin: "1rem" }} raised className={styles.banner}>
+      <Grid
+        style={{ margin: "1rem" }}
+        container
+        spacing={6}
+        className={styles.bannerGrid}
+      >
+        {items}
+      </Grid>
+    </Card>
+  );
 }
 
 const Slider = (props) => {
+  let productArray = [];
+  const products = [];
+  props.products.forEach((product, index) => {
+    if (index % 3 === 0 && productArray.length > 0) {
+      products.push(productArray);
+      productArray = [];
+    } else if (props.products.length === index + 1) {
+      productArray.push(product);
+      products.push(productArray);
+      return false;
+    }
+    productArray.push(product);
+  });
 
-    let productArray = [];
-    const products = []
-    props.products.map((product,index) =>{
-        if(index%3 === 0 && productArray.length>0){
-            products.push(productArray)
-            productArray = []
-        }else if(props.products.length === index+1){
-            productArray.push(product)
-            products.push(productArray)
-            return false
-        }
-        productArray.push(product)
-        
-    })
-
-      return (
-              <Carousel
-                  className="Example"
-                  autoPlay={true}
-                  navButtonsAlwaysVisible={false}
-              >
-                  {
-                      products.map((item, index) => {
-                          return <Banner item={item} onClick={props.onClick} key={index} contentPosition={item.contentPosition} />
-                      })
-                  }
-              </Carousel>
-
-      )
-  }
- export default Slider;
+  return (
+    <Carousel
+      autoPlay={false}
+      navButtonsAlwaysVisible={false}
+    >
+      {products.map((item, index) => {
+        return (
+          <Banner
+            item={item}
+            onClick={props.onClick}
+            key={index}
+            contentPosition={item.contentPosition}
+          />
+        );
+      })}
+    </Carousel>
+  );
+};
+export default Slider;
--- frontend/src/views/Table/Table.js
+++ frontend/src/views/Table/Table.js
@@ -56,7 +56,6 @@ const TableView = ({ className, ...rest }) => {
         (<Card className={clsx(classes.root, className)} {...rest}>
         <CardContent>
         <Box alignItems="center" display="flex" flexDirection="column">
-          {/* <Avatar className={classes.avatar} src={user.avatar} /> */}
           <Typography color="textPrimary" gutterBottom variant="h3">
             {table.name}
           </Typography>
